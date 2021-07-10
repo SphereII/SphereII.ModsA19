8fe9a87a45ef3ec9f9578e14d658356019010fbb
Karl Giesing <karl.giesing001@umb.edu>
2021-07-09 21:36:48 -0400

    #3: NPC additions
    * Modified EAISetAsTargetIfHurt to call base class; refactored
    * Added HumanSleepersAwaken, a Harmony patch to wake up EntityNPCs in sleeper volumes (otherwise they won't wake up unless they're attacking the player)
    * Added DMT drop-in replacement for ObjectiveRandomPoiGoto, that also allows setting names of included and/or excluded prefabs

---
+++ 0-SphereIICore/Harmony/EntityAlive/HumanSleepersAwaken.cs
@@ -0,0 +1,26 @@
+using DMT;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+/// <summary>
+/// Human sleepers are automatically awoken when they become active.
+/// </summary>
+public class HumanSleepersAwaken : IHarmony
+{
+    /// <summary>
+    /// Harmony postfix patch of the EntityAlive.SetSleeperActive method.
+    /// If the entity is an EntityNPC or subclass, it is woken up.
+    /// </summary>
+    [HarmonyPatch(typeof(EntityAlive), "SetSleeperActive")]
+    public class EntityAlive_SetSleeperActive
+    {
+        public static void Postfix(EntityAlive __instance)
+        {
+            if (__instance is EntityNPC)
+                __instance.ConditionalTriggerSleeperWakeUp();
+        }
+    }
+}
